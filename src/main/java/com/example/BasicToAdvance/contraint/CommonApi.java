1b5def83658a4a8664e71ff1ebb0e595171c9a81
Nazmul-dot <nazmulhaq606845@gmail.com>
2025-05-08 21:43:00 +0600

    change in common

--- src/main/java/com/example/BasicToAdvance/contraint/CommonApi.java
+++ src/main/java/com/example/BasicToAdvance/contraint/CommonApi.java
@@ -7,4 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 public abstract class CommonApi {
     public static final String BASE_URL = "/api";
     public static final String BASE_URL_OAUTH2 = "/api/oauth2";
+    public static final String BASE_URL_AUTH_USER = "/api/auth/user";
 }
